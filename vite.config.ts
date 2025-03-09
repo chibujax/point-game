3b77df7dbd86a9908360ab2ffe17f11a3ff4237f
Glitch (uap-voting-game) <none>
2025-03-09 15:44:50 +0000

    📊🤘 Checkpoint ./vite.config.ts:69064286/176 ./package.json:69064286/2

--- vite.config.ts
+++ vite.config.ts
@@ -53,14 +53,5 @@ export default defineConfig({
 				ws: true,
 			},
 		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		coverage: {
-			provider: 'v8',
-			reporter: ['text', 'json', 'html'],
-			exclude: ['node_modules/', 'src/test/'],
-		},
-	},
+	}
 });
