dcb72b81425597efb390ab288515de8e4c248a81
Glitch (uap-voting-game) <none>
2025-07-09 12:49:59 +0100

    fix origin

--- src/config.ts
+++ src/config.ts
@@ -4,13 +4,11 @@ const DEFAULT_WS_URL = 'http://localhost:3000';
 export const config = {
 	apiUrl:
 		process.env.NODE_ENV === 'production'
-			? import.meta.env.VITE_API_URL || DEFAULT_API_URL
+			? window.location.origin
 			: DEFAULT_API_URL,
 
 	wsUrl:
 		process.env.NODE_ENV === 'production'
-			? import.meta.env.VITE_WS_URL || DEFAULT_WS_URL
+			? window.location.origin
 			: DEFAULT_WS_URL,
 } as const;
-
-export type Config = typeof config;
